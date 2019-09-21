89212e2c80bd76d7a5f96470f681ec11e6e84c86
Chris Barker <cbarker@puppet.com>
2019-09-20 17:18:18 -0700

    adds quick workaround for puppet 4 compatibility
    before this, puppet 4 installs would fail due to lack of job_id or code_id in the report format.
    
    could be done better (more like the hash merge used in the facts indirector based on report version)

--- lib/puppet/reports/splunk_hec.rb
+++ lib/puppet/reports/splunk_hec.rb
@@ -27,6 +27,19 @@ Puppet::Reports.register_report(:splunk_hec) do
       },
     }
 
+    # puppet 4 compatibility, code_id and job_id were added in puppet 5
+    if defined?(job_id)
+      local_job_id = job_id
+    else
+      local_job_id = ''
+    end
+
+    if defined?(code_id)
+      local_code_id = code_id
+    else
+      local_code_id = ''
+    end
+
     event = {
       'host' => host,
       'time' => epoch,
@@ -35,11 +48,11 @@ Puppet::Reports.register_report(:splunk_hec) do
         'cached_catalog_status' =>  cached_catalog_status,
         'catalog_uuid' => catalog_uuid,
         'certname' => host,
-        'code_id' => code_id,
+        'code_id' => local_code_id,
         'configuration_version' => configuration_version,
         'corrective_change' => corrective_change,
         'environment' => environment,
-        'job_id' => job_id,
+        'job_id' => local_job_id,
         'metrics' => metrics,
         'noop' => noop,
         'noop_pending' => noop_pending,
